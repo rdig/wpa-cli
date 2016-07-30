217c10a8af81b0d5668c38cb9b813eb87277c749
Raul Glogovetan <raul@glogovetan.com>
2016-07-30 22:37:40 +0300

    package setup & initialization

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * Since we don't have a programatic API just yet, the only thing the package does if it's
+ * imported, is warn the user about this.
+ */
+
+module.exports = function() {
+	console.log('`wordpress-update-cli` does not yet have an programatic interface, it\'s just ' +
+	'used via the CLI. If you want to help build it, we would gladly welcome a PR. Take a look ' +
+	'at https://github.com/rdig/wordpress-update-cli/contributing.md for more info.');
+};
