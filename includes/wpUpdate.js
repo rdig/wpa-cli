8d0509f335e3810bdab832f593d97d947b4d093a
Raul Glogovetan <raul@glogovetan.com>
2016-07-31 01:28:51 +0300

    formatPath(): add trailing slash to given path

--- includes/wpUpdate.js
+++ includes/wpUpdate.js
@@ -3,6 +3,22 @@ var fs = require('fs');
 
 module.exports = {
 	/**
+	 * Add a trailing slash to the supplied path. If there is already one passed in, do nothing.
+	 *
+	 * @method formatPath
+	 *
+	 * @param {string} path The path to the wordpress installation (or any other path)
+	 *
+	 * @return {string} The path with the trailing slash added / the original path
+	 */
+	formatPath: function(path) {
+		path = path || '/';
+		if (path.indexOf('/') !== path.length-1) {
+			return path + '/';
+		}
+		return path;
+	},
+	/**
 	 * @todo Find a reliable way to parse the versions file
 	 *
 	 * Check the version of the locally installed wordpress
