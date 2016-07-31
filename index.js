816a52839e0bcbd5056b88d7bd32bdd2d9a39199
Raul Glogovetan <raul@glogovetan.com>
2016-07-31 17:52:40 +0300

    changed default import message / added it to messages.json

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+var packageJson = require('package.json');
+var msgs = require('includes/messages.json');
+
 /*
  * Since we don't have a programatic API just yet, the only thing the package does if it's
- * imported, is warn the user about this.
+ * imported, is to warn the user about this.
  */
-
 module.exports = function() {
-	console.log('`wpa-cli` does not yet have an programatic interface, it\'s just ' +
-	'used via the CLI. If you want to help build it, we would gladly welcome a PR. Take a look ' +
-	'at https://github.com/rdig/wpa-cli/contributing.md for more info.');
+	console.log('`' + packageJson.name + '` ' + msgs.noApi);
 };
