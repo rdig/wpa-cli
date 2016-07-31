95b2639ac6bc7d4b122a61c9e46304d9ebe4af32
Raul Glogovetan <raul@glogovetan.com>
2016-07-31 07:14:26 +0300

    commented the starter file

--- bin/wpu_cli.js
+++ bin/wpu_cli.js
@@ -4,10 +4,19 @@ var cli = require('cli');
 var packageJson = require(__dirname + '/../package.json');
 var wpu = require('../includes/wpu.js');
 
+/*
+ * Set the app name and version, otherwise it defaults to the ones from the `cli` package
+ */
 cli.setApp('wpu', packageJson.version);
 
+/*
+ * Enable plugins
+ */
 cli.enable('version', 'status');
 
+/*
+ * Describe the required options
+ */
 cli.parse({
 	path: ['p', 'Path where your Wordpress install resides', 'path']
 });
